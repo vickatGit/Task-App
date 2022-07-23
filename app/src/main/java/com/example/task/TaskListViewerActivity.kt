525fe2639dd3c006eaadd27f8bcb3c7ee6f948ca
vikas <gangishettyvikas@gmail.com>
2022-07-23 23:50:06 +0530

    activity created for viewing tasklist

---
+++ app/src/main/java/com/example/task/TaskListViewerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.task
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TaskListViewerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_list_viewer)
+    }
+}
